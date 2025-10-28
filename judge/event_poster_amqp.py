dde8fa52e0435a416c434800b3c183d6a5dee7d6
Vương Tuấn Vinh <vtvinh24@gmail.com>
2025-10-28 09:32:05 +0700

    Update event_poster_amqp.py
    Add logging

--- judge/event_poster_amqp.py
+++ judge/event_poster_amqp.py
@@ -3,10 +3,14 @@ import threading
 from time import time
 
 import pika
+import logging
 from django.conf import settings
 from pika.exceptions import AMQPError
 
 __all__ = ['EventPoster', 'post', 'last']
+
+# Module logger for AMQP event poster
+logger = logging.getLogger('judge.event_poster_amqp')
 
 
 class EventPoster(object):
@@ -15,19 +19,34 @@ class EventPoster(object):
         self._exchange = settings.EVENT_DAEMON_AMQP_EXCHANGE
 
     def _connect(self):
-        self._conn = pika.BlockingConnection(pika.URLParameters(settings.EVENT_DAEMON_AMQP))
-        self._chan = self._conn.channel()
+        try:
+            self._conn = pika.BlockingConnection(pika.URLParameters(settings.EVENT_DAEMON_AMQP))
+            self._chan = self._conn.channel()
+            logger.info('Connected to AMQP broker %s (exchange=%s)', settings.EVENT_DAEMON_AMQP, self._exchange)
+        except AMQPError:
+            logger.exception('Failed to connect to AMQP broker %s', settings.EVENT_DAEMON_AMQP)
+            raise
 
     def post(self, channel, message, tries=0):
         try:
             id = int(time() * 1000000)
-            self._chan.basic_publish(self._exchange, '',
-                                     json.dumps({'id': id, 'channel': channel, 'message': message}))
+            payload = json.dumps({'id': id, 'channel': channel, 'message': message})
+            # Note: routing_key is currently empty string; exchange bindings determine delivery.
+            self._chan.basic_publish(self._exchange, '', payload)
+            logger.debug('Published event id=%s channel=%s exchange=%s size=%d', id, channel, self._exchange, len(payload))
             return id
         except AMQPError:
+            logger.warning('AMQP publish error on try %d for channel %s; attempting reconnect', tries, channel)
             if tries > 10:
+                logger.error('AMQP publish failed after %d retries for channel %s', tries, channel)
                 raise
-            self._connect()
+            # attempt reconnect and retry
+            try:
+                self._connect()
+            except AMQPError:
+                # connection attempt failed; bubble up to caller after logging
+                logger.exception('Reconnect attempt failed during publish retry')
+                raise
             return self.post(channel, message, tries + 1)
 
 
@@ -36,6 +55,7 @@ _local = threading.local()
 
 def _get_poster():
     if 'poster' not in _local.__dict__:
+        logger.debug('Creating thread-local EventPoster')
         _local.poster = EventPoster()
     return _local.poster
 
@@ -44,6 +64,7 @@ def post(channel, message):
     try:
         return _get_poster().post(channel, message)
     except AMQPError:
+        logger.exception('AMQPError in post(); discarding poster and returning 0')
         try:
             del _local.poster
         except AttributeError:
